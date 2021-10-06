6ebd59d8ecb51cafa67ab0d46876a2b5b2610c4d
Logger <logger@Loggerui-MacPro.local>
2021-10-07 04:44:17 +0900

    json reader, developchapter[thumbnail, label, type, desc]

--- src/plugin.js
+++ src/plugin.js
@@ -27,6 +27,22 @@ const onPlayerReady = (player, options) => {
   console.log("Chapters : ",options)
 };
 
+var getJSON = function(url, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  //xhr.responseType = 'json';
+  xhr.onload = function() {
+    var status = xhr.status;
+    console.log(xhr)
+    if (status === 200) {
+      callback(null, xhr.response);
+    } else {
+      callback(status, xhr.response);
+    }
+  };
+  xhr.send();
+};
+
 /**
  * A video.js plugin.
  *
@@ -41,10 +57,124 @@ const onPlayerReady = (player, options) => {
  */
 const chaptersMore = function(options) {
   this.ready(() => {
+
     console.log("Plugin Started");
+    var player = this;
+    getJSON(options.src, function(err, data) {
+    
+      if (err != null) {
+          console.error(err);
+      } else {    
+          var parsedData = JSON.parse(data)
+
+          if(parsedData.length > 0){
+
+              let chapterList = document.createElement("div");
+              chapterList.className = "vjs-chapters-more-list fade-out-box ";
+              
+              parsedData.map(function(item, idx){
+                console.log( parsedData[idx]['type'] )
+                var chapterBox = createChapterBox(player, parsedData[idx]);
+                chapterList.appendChild(chapterBox);
+              });
+
+
+              var controlBarHeight = player.controlBar.currentHeight();     
+              player.on('playerresize' ,function(){//chapter panel resize
+                var playerHeight = player.currentHeight();
+                console.log(  "controlbar : ", player.controlBar )
+                chapterList.style.height = (playerHeight - controlBarHeight) + "px";
+                
+              });
+                    
+              chapterList.style.bottom = controlBarHeight + "px";
+              player.el().appendChild(chapterList); 
+
+
+              // create chapter custom button
+              var btnChapters = player.controlBar.addChild("button");
+
+              var btnChaptersDom = btnChapters.el();
+              btnChaptersDom.innerHTML = '<span class="vjs-icon-placeholder" aria-hidden="true"></span>';
+              btnChapters.addClass("vjs-button");
+              btnChapters.addClass("vjs-chapters-button");
+              chapterList.style.display = 'none';
+              btnChaptersDom.onclick = function(){
+                  if( chapterList.classList.contains('fade-in-box') ){
+                    
+                    setTimeout(function(){
+                      chapterList.style.display = 'none';
+                    }, 500);                    
+                    chapterList.classList.remove("fade-in-box");
+                    chapterList.classList.add("fade-out-box");
+                  }else{
+                    chapterList.classList.add("fade-in-box");
+                    chapterList.classList.remove("fade-out-box");
+                    chapterList.style.display = '';
+                    
+                  }
+
+              }  
+
+
+          }
+      }
+    });
+
     onPlayerReady(this, videojs.mergeOptions(defaults, options));
   });
 };
+
+function createChapterBox(player, item){
+  console.log(item)
+  let chapterBox = document.createElement("div");
+  chapterBox.className = 'box-chapter';
+  var boxContent = document.createElement('div');;
+  
+  var thumbnail = document.createElement('div');
+  thumbnail.className = 'chapter-thumbnail fl'
+  thumbnail.style.backgroundImage = "url('" + item.thumbnail + "')";
+  boxContent.appendChild(thumbnail);
+
+  //define clear box
+  var clearBox = document.createElement('div');
+  clearBox.className = "cr";
+  var innerBox = document.createElement('div');
+  innerBox.className = "fl";
+
+  var boxLabel = document.createElement('div');
+  boxLabel.className = "chapter-label"
+  var labelContent = document.createTextNode(item.label);
+  boxLabel.appendChild(labelContent);
+  
+  var boxDesc = document.createElement('div');
+  boxDesc.className = "chapter-desc"
+  var desclContent = document.createTextNode(item.desc);
+  boxDesc.appendChild(desclContent);
+
+  innerBox.appendChild(boxLabel);
+  innerBox.appendChild(boxDesc);
+
+  boxContent.appendChild(innerBox);
+
+  switch(item.type){
+    case "text":
+      chapterBox.addEventListener( 'click', function(){
+        player.currentTime(item.start);
+      });      
+    break;
+    case "link":
+      chapterBox.addEventListener( 'click', function(){
+        window.open(item.href);
+      });
+    break;
+
+  }
+  boxContent.appendChild(clearBox);
+
+  chapterBox.appendChild(boxContent);
+  return chapterBox;
+}
 
 // Register the plugin with video.js.
 registerPlugin('chaptersMore', chaptersMore);
